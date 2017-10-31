55cca33270d393508593c52c2c1b5fa246a47c57
Justin Fuller <justindanielfuller@gmail.com>
2017-10-30 21:43:30 -0400

    Added logging to debug startup time

--- src/index.js
+++ src/index.js
@@ -26,9 +26,16 @@ const accumulator = (app, method) => mergeFreeze(
   method(app)
 );
 
+const startDate = new Date();
+
 /**
  * reduceRight will work the same as fp.compose (right to left).
  * The result of each function will be merged with the result of the next.
  * So each function doesn't have to worry about modifying or adding the lasts result.
  */
 _.reduceRight(composedApp, accumulator, {});
+
+const endDate   = new Date();
+const seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+
+console.log(`App started in ${seconds} seconds`);
